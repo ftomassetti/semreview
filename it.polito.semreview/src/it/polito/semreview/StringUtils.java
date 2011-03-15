031fd6228aae37a2a18e601b381b7fd3132a71f4
Federico Tomassetti <federico.tomassetti@polito.it>
2011-03-15 18:50:20 +0100

    OpenCalais integration revised

---
+++ it.polito.semreview/src/it/polito/semreview/StringUtils.java
@@ -0,0 +1,6 @@
+package it.polito.semreview;
+
+public class StringUtils {
+	public static String repulisti(String original){
+		re	}
+}
---
+++ it.polito.semreview/src/it/polito/semreview/dataset/TextFileDirDataSetProvider.java
@@ -0,0 +1,94 @@
+package it.polito.semreview.dataset;
+
+import it.polito.semreview.utils.filesystem.FileNameExtensionFilter;
+import it.polito.semreview.utils.filesystem.FileUtils;
+import it.polito.softeng.common.StringUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This class examine a directory and its subdirectories to find text files
+ * which are loaded and Paper are created.
+ */
+public class TextFileDirDataSetProvider implements DataSetProvider {
+	
+	private static Logger logger = Logger.getLogger(TextFileDirDataSetProvider.class);
+
+	private File rootDir;
+	private String journalName;
+
+	public TextFileDirDataSetProvider(File rootDir, String journalName) {
+		if (!rootDir.exists() || !rootDir.isDirectory()) {
+			throw new IllegalArgumentException(
+					"Given path should exist and being a directory");
+		}
+		this.rootDir = rootDir;
+		this.journalName = journalName;
+	}
+
+	@Override
+	public List<Paper> getAllPapers() throws IOException {
+		List<Paper> papers = new LinkedList<Paper>();
+		int yearsCount = 0;
+		int issuesCount = 0;
+		int papersCount = 0;
+		for (File yearDir : rootDir.listFiles()) {
+			try {
+				int year = Integer.parseInt(yearDir.getName());
+				yearsCount++;
+				for (File issueDir : yearDir.listFiles()) {
+					if (issueDir.isDirectory()
+							&& issueDir.getName().startsWith("issue_")) {
+						try {
+							int issue = Integer.parseInt(issueDir.getName()
+									.substring("issue_".length()));
+							for (File paperFile : issueDir.listFiles()) {
+								if (paperFile.isFile()&&paperFile.getName().endsWith(".txt")) {
+									String title = paperFile.getName();
+									title = StringUtils.removePostfix(title,
+											".txt");
+									PaperId paperId = new PaperId(
+											this.journalName, year, issue,
+											title);
+									papers.add(load(paperId, paperFile));
+									papersCount++;
+								}
+							}
+							issuesCount++;
+							logger.debug("Year " + year + ", issue "
+									 + issue+" (issues done: "+issuesCount+")");
+						} catch (NumberFormatException e) {
+							logger.debug("...skipping "
+									+ issueDir.getAbsolutePath());
+						}
+
+					} else {
+						logger.debug("...skipping "
+								+ issueDir.getAbsolutePath());
+					}
+				}
+			} catch (NumberFormatException e) {
+				// skip the subdir
+			}
+		}
+		logger.debug("Years: " + yearsCount + ", issues " + issuesCount+", papers "+papersCount);
+		/*
+		 * for (File textFile : FileUtils.listFile(rootDir,new
+		 * FileNameExtensionFilter("txt"), true)){ papers.add(load(textFile)); }
+		 */
+		return papers;
+	}
+
+	private Paper load(PaperId paperId, File textFile) throws IOException {
+		PaperImpl paper = new PaperImpl(paperId);
+		String content = FileUtils.readFile(textFile);
+		paper.addSection("fullText", content);
+		return paper;
+	}
+
+}
