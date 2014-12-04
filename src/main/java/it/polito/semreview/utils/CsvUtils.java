dfccac59ab64d6228b73130abe38bf3fdb06b928
Federico Tomassetti <f.tomassetti@gmail.com>
2014-12-04 19:37:33 +0000

    Removing unused code

--- src/main/java/it/polito/semreview/utils/CsvUtils.java
+++ src/main/java/it/polito/semreview/utils/CsvUtils.java
@@ -9,12 +9,5 @@ public final class CsvUtils {
 	public static String serializeForCsv(String originalText){
 		return originalText.replaceAll("\'", "''").replaceAll("\n", "");
 	}
-	
-	public static String unserializeForCsv(String serializedText){
-		String text = serializedText.substring(1);
-		text = text.substring(0,text.length()-1);
-		text = text.replaceAll("\'\'","'");
-		return text;
-	}
 
 }
