455d641994373326627ccbbd07b226a40249c358
Federico Tomassetti <f.tomassetti@gmail.com>
2014-12-07 19:43:59 +0000

    Typos

--- src/main/java/it/polito/semreview/dbpedia/InvalidResponseException.java
+++ src/main/java/it/polito/semreview/dbpedia/InvalidResponseException.java
@@ -5,11 +5,11 @@ public class InvalidResponseException extends Exception {
 	private static final long serialVersionUID = -2443243290009090744L;
 
 	public InvalidResponseException(String reason) {
-		super("Unvalid response obtained: " + reason);
+		super("Invalid response obtained: " + reason);
 	}
 
 	public InvalidResponseException(String reason, Throwable cause) {
-		super("Unvalid response obtained: " + reason, cause);
+		super("Invalid response obtained: " + reason, cause);
 	}
 
 }
