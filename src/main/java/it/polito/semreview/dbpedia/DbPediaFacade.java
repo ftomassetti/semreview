93e6ce355147b944bcaed6b6cfa4fe8fd23ffa37
Federico Tomassetti <f.tomassetti@gmail.com>
2014-12-07 19:32:44 +0000

    Commenting DbPediaFacade

--- src/main/java/it/polito/semreview/dbpedia/DbPediaFacade.java
+++ src/main/java/it/polito/semreview/dbpedia/DbPediaFacade.java
@@ -6,8 +6,30 @@ import java.net.URI;
 import javax.xml.parsers.ParserConfigurationException;
 
 public interface DbPediaFacade {
+
+    /**
+     * Retrieve the abstract describing the given keyphrase.
+     *
+     * @param keyphrase
+     * @return
+     * @throws NoResourceFoundException
+     * @throws ParserConfigurationException
+     * @throws IOException
+     * @throws UnvalidResponseException
+     * @throws UnvalidDefinitionException
+     */
 	String retrieveAbstract(String keyphrase) throws NoResourceFoundException,
 			ParserConfigurationException, IOException,
 			UnvalidResponseException, UnvalidDefinitionException;
+
+    /**
+     * Retrieve the abstract associated to the given URI.
+     * @param expectedURI
+     * @return
+     * @throws IOException
+     * @throws UnvalidResponseException
+     * @throws UnvalidDefinitionException
+     * @throws ParserConfigurationException
+     */
 	String retrieveAbstractFromURI(URI expectedURI) throws IOException, UnvalidResponseException, UnvalidDefinitionException, ParserConfigurationException;
 }
